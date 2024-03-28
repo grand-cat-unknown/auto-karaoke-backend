bc61f95df545a8447c2a8ece9800263432336ccc
srikanthganta182 <srikanth.ganta@ixor.be>
2024-03-28 17:31:32 +0100

    v1

--- video_generator.py
+++ video_generator.py
@@ -7,18 +7,26 @@ class KaraokeScene(Scene):
         # Load lyrics
         with open('lyrics.txt') as f:
             lyrics = [line.strip().split() for line in f.readlines()]
+        
+        # remove emptylists from lyrics
+        lyrics = [line for line in lyrics if line]
 
         # Load timestamps
         timestamp_json = json.load(open('timestamps.json'))
         timestamps = []
-
+        song_length = timestamp_json['nonspeech_sections'][-1]['end']
         # Process timestamps
         for segment in timestamp_json['segments']:
             for word in segment['words']:
                 start = word['start']
                 end = word['end']
-                word_text = re.sub(r"\s+", "", word['word'])  # Clean word text
-                timestamps.append((start, end, word_text))
+                # if next word doesn't start with a space, append it to the previous word
+                if not word['word'].startswith(' '):
+                    start = timestamps[-1][1]
+                    timestamps[-1] = (start, end, timestamps[-1][2] + word['word'])
+                else:
+                    word_text = re.sub(r"\s+", "", word['word'])  # Clean word text
+                    timestamps.append((start, end, word_text))
 
         # Sync lyrics with timestamps
         i = 0
@@ -31,20 +39,31 @@ class KaraokeScene(Scene):
                 start, end, text = timestamps[i]
                 one_line.append((start, end, text))
                 i += 1
+            print(line)
+            print(one_line)
             synced_lyrics.append(one_line)
 
-        for sentence in synced_lyrics[:3]:
-            self.clear()
+        # Display lyrics
 
+        previous_end_time = 0  # Keep track of the end time of the last word
+        for sentence in synced_lyrics:
+            if not sentence:  # Skip empty sentences
+                continue
+            self.clear()
             line_group = VGroup()  # Group for the current line of lyrics
+
             for word_info in sentence:
                 start, end, word_text = word_info
                 word = Text(word_text, color=WHITE).scale(0.7)
                 line_group.add(word)  # Add the word to the line group
-            
+
             line_group.arrange(RIGHT, buff=0.1)  # Arrange words in a row
             self.add(line_group)  # Display the line
-            
+
+            pause_duration = sentence[0][0] - previous_end_time  # Calculate pause duration
+            if pause_duration > 0:
+                self.wait(pause_duration)  # Wait for the duration of the pause
+
             # Highlight words sequentially
             for i, word_info in enumerate(sentence):
                 start, end, _ = word_info
@@ -53,3 +72,9 @@ class KaraokeScene(Scene):
                     self.add(line_group[i].set_color(YELLOW))
                 else:  # For non-zero durations, proceed with normal animation
                     self.play(line_group[i].animate.set_color(YELLOW), run_time=duration)
+            
+            previous_end_time = sentence[-1][1]
+        # Wait for the remaining duration
+        remaining_duration = song_length - previous_end_time
+        if remaining_duration > 0:
+            self.wait(remaining_duration)
